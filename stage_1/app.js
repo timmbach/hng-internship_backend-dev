bbae329a9f2db2a626cdbd6fac6f32861201bfd1
timmbach <follow.timothy@gmail.com>
2023-09-07 16:39:36 +0100

    adjusted utc time

--- stage_1/app.js
+++ stage_1/app.js
@@ -17,15 +17,15 @@ const daysOfWeek = [
   "Friday",
   "Saturday",
 ];
-const currentDate = new Date();
-const currentDay = daysOfWeek[currentDate.getDay()];
+// const currentDate = new Date();
+// const currentDay = daysOfWeek[currentDate.getDay()];
 
 app.get("/api", (req, res) => {
   console.log(req.query);
   return res.status(200).json({
     slack_name: req.query.slack_name,
-    current_day: currentDay,
-    utc_time: currentDate.toISOString().replace(/\.\d+/, ""),
+    current_day: daysOfWeek[new Date().getDay()],
+    utc_time: new Date().toUTCString().replace(/\.\d+/, ""),
     track: req.query.track,
     github_file_url:
       "https://github.com/timmbach/hng_internship/blob/master/stage_1/app.js",
@@ -35,5 +35,6 @@ app.get("/api", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  // console.log(`Server is listening on port ${PORT}`);
+  console.log(currentDate.toISOString().replace(/\.\d+/, ""));
 });
